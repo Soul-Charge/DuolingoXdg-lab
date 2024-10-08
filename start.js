56fe777d4fa2f75234ca243fdb96699eefc7f91c
Soul-Charge <soul-charge@outlook.com>
2024-10-08 20:52:20 +0800

    v-0.1
    完成多邻国的页面打开，在cookies.txt中填写cookies，为了后续开发方便暂不上次该文件（我的填进去了）
    完成多邻国做题时的错误检测，并且可以在触发错误后运行其他代码

---
+++ start.js
@@ -0,0 +1,96 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs').promises;
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--window-size=1920,1080'],
+  }); // 启动浏览器
+  const page = await browser.newPage();
+
+  // 从文件中读取cookie并设置，注意使用await等待该函数执行完成
+  const cookies = await readCookiesFromFile('./cookies.txt');
+  await setCookies(page, cookies);
+
+  // 关闭第一个默认打开的空白页面
+  const pages = await browser.pages();
+  if (pages.length > 0) {
+    await pages[0].close();
+  }
+
+  //调整页面大小
+  await page.setViewport({ width: 1920, height: 960 });
+  // 打开多邻国页面
+  await page.goto('https://www.duolingo.com/');
+
+  // 检测错误图片的函数
+  await detectErrorImage(page);
+  // 检测到回答错误后运行的函数
+  await OnWrongAnswer();
+
+})();
+
+
+
+// 从文件中读取cookies的函数
+async function readCookiesFromFile(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    // 确保数据按行分割，并解析成对象数组
+    const cookies = data.split('\n').map(line => {
+      const [name, value] = line.trim().split('=');
+      return { name, value, domain: '.duolingo.com' }; // 确保domain属性与目标网站匹配
+    }).filter(cookie => cookie.name && cookie.value);
+
+    console.log('Parsed cookies:', cookies); // 调试输出，查看解析结果
+    return cookies;
+  } catch (error) {
+    console.error('Error reading cookies from file:', error);
+    return [];
+  }
+}
+
+// 设置一个page变量的cookies的函数
+async function setCookies(page, cookies) {
+  for (const cookie of cookies) {
+    await page.setCookie(cookie);
+  }
+}
+
+async function detectErrorImage(page) {
+  return new Promise((resolve) => {
+    // 创建一个 MutationObserver 实例
+    page.evaluate(() => {
+      return new Promise((observerResolve) => {
+        const observer = new MutationObserver((mutations) => {
+          // 遍历变更记录
+          for (const mutation of mutations) {
+            if (mutation.type === 'childList') {
+              // 查找错误提示图片
+              const errorImage = document.querySelector('img._1gpzD[src="https://d35aaqx5ub95lt.cloudfront.net/images/9a4bf74a74e801ca35402f2c2837e24c.svg"]');
+              if (errorImage) {
+                observerResolve(); // 解析 Promise
+                break;
+              }
+            }
+          }
+        });
+
+        // 配置观察者的选项
+        observer.observe(document.body, {
+          childList: true,
+          subtree: true, // 观察所有子节点的变化
+        });
+      });
+    }).then(resolve); // 在 Promise 解析后回调 resolve
+  });
+}
+
+async function OnWrongAnswer() {
+  console.log('回答错误');
+}
+
+// 函数实现，参数单位 毫秒 ；
+function wait(ms) {
+    return new Promise(resolve =>setTimeout(() =>resolve(), ms));
+};
